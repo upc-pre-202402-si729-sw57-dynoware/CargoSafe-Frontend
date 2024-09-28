c5f2f3f2b13359538810d88260320288fd92c625
rivacortezf <u202215313@upc.edu.pe>
2024-09-28 10:50:52 -0500

    feat: added fake api environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  serverBasePath: 'http://localhost:3000/api/v1',
+  //serverBasePath: 'http://localhost:3000/api/v1',
+  serverBasePath:'https://cargosafe-fake-api.vercel.app'
 };
