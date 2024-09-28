32e4b637552bdbee0bccdfb1dbd6ed8ed5271081
rrenzosilva <149326018+rrenzosilva@users.noreply.github.com>
2024-09-27 22:23:01 -0500

    feat(merchandise): add merchandise entity

---
+++ src/app/merchandise/model/merchandise.entity.spec.ts
@@ -0,0 +1,7 @@
+import { MerchandiseEntity } from './merchandise.entity';
+
+describe('MerchandiseEntity', () => {
+  it('should create an instance', () => {
+    expect(new MerchandiseEntity()).toBeTruthy();
+  });
+});
