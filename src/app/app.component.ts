1c2e4c45938d1ca70e0cdda9505cd3e50cfc8243
rrenzosilva <149326018+rrenzosilva@users.noreply.github.com>
2024-09-27 22:19:06 -0500

    chore: project setup.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MerchandiseItemComponent} from "./merchandise/components/merchandise-item/merchandise-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MerchandiseItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
