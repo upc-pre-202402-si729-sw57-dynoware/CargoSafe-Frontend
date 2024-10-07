8b88fbd89a67c0e8e6b2919c56bdc34f54de3849
rivacortezf <u202215313@upc.edu.pe>
2024-10-07 00:36:05 -0500

    feat: add role entity

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  serverBasePath:'https://cargosafe-fake-api.vercel.app'
+  //serverBasePath:'https://cargosafe-fake-api.vercel.app'
+  serverBasePath:'http://localhost:3000'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  serverBasePath:'https://cargosafe-fake-api.vercel.app'
+  //serverBasePath:'https://cargosafe-fake-api.vercel.app'
+  serverBasePath:'http://localhost:3000'
 };
