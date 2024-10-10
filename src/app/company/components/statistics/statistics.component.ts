a23a93e6c7fa3c564c92fc2f8fe091c4411bf60b
Astrid Boronda Heidinger <108700549+AstridBH@users.noreply.github.com>
2024-10-10 16:35:27 -0500

    feat: add additional charts to data statistics

--- src/app/company/components/statistics/statistics.component.ts
+++ src/app/company/components/statistics/statistics.component.ts
@@ -3,13 +3,21 @@ import { StatisticsResponse, StadisticsService } from "../../services/stadistics
 import { MatGridList, MatGridTile } from "@angular/material/grid-list";
 import { StatisticsEntity } from "../../model/statistics.entity";
 import { Chart, registerables } from 'chart.js';
+import {MatCard, MatCardContent, MatCardFooter, MatCardTitle} from "@angular/material/card";
+import {TranslateModule, TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-statistics',
   standalone: true,
   imports: [
     MatGridList,
-    MatGridTile
+    MatGridTile,
+    MatCard,
+    MatCardContent,
+    TranslateModule,
+    MatCardTitle,
+    MatCardFooter,
+
   ],
   templateUrl: './statistics.component.html',
   styleUrl: './statistics.component.css'
@@ -17,9 +25,9 @@ import { Chart, registerables } from 'chart.js';
 export class StatisticsComponent implements OnInit {
   chart: any;
 
-  constructor(private statisticsService: StadisticsService) {
-    // Registra todos los componentes necesarios de Chart.js
+  constructor(private statisticsService: StadisticsService, private translate: TranslateService) {
     Chart.register(...registerables);
+    this.translate.setDefaultLang('en');
   }
 
   ngOnInit(): void {
@@ -28,23 +36,54 @@ export class StatisticsComponent implements OnInit {
         const statistics = new StatisticsEntity(response[0].data); // Crear una nueva instancia de StatisticsEntity
         this.loadChart(statistics.data); // Accede a los datos
       }
+
     });
   }
 
   loadChart(chartData: any): void {
-    this.createChart('load-chart-1', 'Número de pedidos por mes', chartData.chart1.labels, chartData.chart1.orders, 'rgba(75, 192, 192, 0.2)', 'rgb(75, 192, 192)');
-    this.createChart('load-chart-2', 'Número de entregas por región', chartData.chart2.labels, chartData.chart2.deliveries, 'rgba(255, 159, 64, 0.2)', 'rgb(255, 159, 64)');
-    this.createChart('load-chart-3', 'Tipos de carga transportada', chartData.chart3.labels, chartData.chart3.cargo, 'rgba(255, 205, 86, 0.2)', 'rgb(255, 205, 86)');
-    this.createChart('load-chart-4', 'Cargas entregadas a tiempo vs retrasadas', chartData.chart4.labels, chartData.chart4.deliveries, 'rgba(153, 102, 255, 0.2)', 'rgb(153, 102, 255)');
+    this.createVerticalBarChart('vertical-chart-1', '', chartData.chart1.labels, chartData.chart1.orders, [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)'],
+      ['rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)']);
+    this.createVerticalBarChart('vertical-chart-2', '', chartData.chart2.labels, chartData.chart2.profits, [
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(201, 203, 207, 0.2)',
+      'rgba(179, 117, 11, 0.2)'],
+      ['rgb(54, 162, 235)',
+        'rgb(153, 102, 255)',
+        'rgb(201, 203, 207)',
+        'rgb(179, 117, 11)']);
+    this.createHorizontalBarChart('horizontal-chart', '', chartData.chart4.labels, chartData.chart4.orders, [
+      'rgba(214, 130, 112, 0.2)',
+      'rgba(214, 181, 112, 0.2)',
+      'rgba(196, 214, 112, 0.2)',
+      'rgba(145, 214, 112, 0.2)',
+      'rgba(112, 214, 181, 0.2)'],
+      ['rgb(214, 130, 112)',
+      'rgb(214, 181, 112)',
+      'rgb(196, 214, 112)',
+      'rgb(145, 214, 112)',
+      'rgb(112, 214, 181)'
+    ]);
+    this.createPieChart('pie-chart', '', chartData.chart3.labels, chartData.chart3.orders, ['rgb(54, 162, 235)',
+      'rgb(255, 205, 86)']);
+    this.createLineChart('line-chart', '', chartData.chart5.labels, chartData.chart5.costs, 'rgb(75, 192, 192)');
+    this.createRadarChart('radar-chart', '', chartData.chart6.labels, chartData.chart6.orders, 'rgba(130, 112, 214  , 0.2)', 'rgb(130, 112, 214)');
   }
 
-  createChart(
+  createVerticalBarChart(
     elementId: string,
     label: string,
     labels: string[],
     data: number[],
-    backgroundColor: string,
-    borderColor: string
+    backgroundColor: string[],
+    borderColor: string[]
   ): void {
     new Chart(elementId, {
       type: 'bar',
@@ -69,4 +108,116 @@ export class StatisticsComponent implements OnInit {
       },
     });
   }
+
+  createHorizontalBarChart(
+    elementId: string,
+    label: string,
+    labels: string[],
+    data: number[],
+    backgroundColor: string[],
+    borderColor: string[]
+  ): void {
+    new Chart(elementId, {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: label,
+            data: data,
+            backgroundColor: backgroundColor,
+            borderColor: borderColor,
+            borderWidth: 3,
+          },
+        ],
+      },
+      options: {
+        indexAxis: 'y',
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  }
+
+  createPieChart(
+    elementId: string,
+    label: string,
+    labels: string[],
+    data: number[],
+    backgroundColor: string[],
+  ): void {
+    new Chart(elementId, {
+      type: 'pie',
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: label,
+            data: data,
+            backgroundColor: backgroundColor,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  }
+
+  createLineChart(elementId: string, label: string, labels: string[], data: number[],borderColor: string): void {
+    new Chart(elementId, {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: label,
+            data: data,
+            borderColor: borderColor,
+            tension: 0.1
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  }
+
+  createRadarChart(elementId: string, label: string, labels: string[], data: number[], backgroundColor: string, borderColor: string): void {
+    new Chart(elementId, {
+      type: 'radar',
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: label,
+            data: data,
+            backgroundColor: backgroundColor,
+            borderColor: borderColor,
+            borderWidth: 3,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  }
+
 }
--- src/app/company/services/stadistics.service.ts
+++ src/app/company/services/stadistics.service.ts
@@ -11,18 +11,37 @@ export interface Chart1 {
 
 export interface Chart2 {
   labels: string[];
-  deliveries: number[];
+  profits: number[];
 }
 
 export interface Chart3 {
   labels: string[];
-  cargo: number[];
+  orders: number[];
 }
+
+export interface Chart4 {
+  labels: string[];
+  orders: number[];
+}
+
+export interface Chart5 {
+  labels: string[];
+  costs: number[];
+}
+
+export interface Chart6 {
+  labels: string[];
+  orders: number[];
+}
+
 
 export interface Data {
   chart1: Chart1;
   chart2: Chart2;
   chart3: Chart3;
+  chart4: Chart4;
+  chart5: Chart5;
+  chart6: Chart6;
 }
 
 // Cambiar el nombre de Response a StatisticsResponse
