de456fe1e90f92d3ce61400e1953f7baa7a7e238
Alexandra Ramos <149441307+AleRamosA@users.noreply.github.com>
2024-10-20 20:48:48 -0500

    feat: add trip-tracking component.

--- src/app/trip/components/trip-tracking/trip-tracking.component.ts
+++ src/app/trip/components/trip-tracking/trip-tracking.component.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
 import {MatLabel} from "@angular/material/form-field";
 import {MatList, MatListItem} from "@angular/material/list";
+import {Merchandise} from "../../../merchandise/model/merchandise.entity";
+import {TripService} from "../../service/trip.service";
+import {MerchandiseService} from "../../../merchandise/services/merchandise.service";
+import {MatButtonToggle} from "@angular/material/button-toggle";
+import {MatGridList, MatGridTile} from "@angular/material/grid-list";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-trip-tracking',
@@ -13,11 +19,33 @@ import {MatList, MatListItem} from "@angular/material/list";
     MatLabel,
     MatList,
     MatListItem,
-    MatCardTitle
+    MatCardTitle,
+    MatButtonToggle,
+    MatGridList,
+    MatGridTile,
+    MatIcon
   ],
   templateUrl: './trip-tracking.component.html',
   styleUrl: './trip-tracking.component.css'
 })
-export class TripTrackingComponent {
+export class TripTrackingComponent implements OnInit{
+  protected merchandise: Merchandise=new Merchandise();
+  private tripsApi: TripService = inject(TripService);
+  private merchandiseApi: MerchandiseService = inject(MerchandiseService);
+  protected id:number=2;
+  protected isStep1Complete = true;
+  protected isStep2Complete = true;
+  protected isStep3Complete = true;
+  protected isStep4Complete = true;
+  protected isStep5Complete = true;
+  ngOnInit(): void {
+    this.merchandiseApi.getById(this.id).subscribe((data: any) => {
+      this.merchandise=data;
 
+    });
+
+    this.tripsApi.getById(this.id).subscribe((data2: any) => {
+      this.merchandise.product=data2.name;
+    })
+  }
 }
