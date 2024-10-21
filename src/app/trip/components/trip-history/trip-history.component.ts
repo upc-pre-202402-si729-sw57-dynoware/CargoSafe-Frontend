691258e2ba7e34dae1537b5452cbd4e13d1c5a8a
rrenzosilva <149326018+rrenzosilva@users.noreply.github.com>
2024-10-21 17:11:53 -0500

    feat: add trip-history-component.html

--- src/app/trip/components/trip-history/trip-history.component.ts
+++ src/app/trip/components/trip-history/trip-history.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
 import {TripEntity} from "../../model/trip.entity";
-import {MatTableDataSource} from "@angular/material/table";
+import {
+  MatCell, MatCellDef,
+  MatColumnDef,
+  MatHeaderCell, MatHeaderCellDef,
+  MatHeaderRow, MatHeaderRowDef, MatRow, MatRowDef,
+  MatTable,
+  MatTableDataSource
+} from "@angular/material/table";
 import {TripService} from "../../service/trip.service";
+import {MatCard} from "@angular/material/card";
+import {MatButton} from "@angular/material/button";
+import {MatPaginator} from "@angular/material/paginator";
 
 @Component({
   selector: 'app-trip-history',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCard,
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatCell,
+    MatButton,
+    MatHeaderRow,
+    MatRow,
+    MatPaginator,
+    MatHeaderCellDef,
+    MatCellDef,
+    MatHeaderRowDef,
+    MatRowDef
+  ],
   templateUrl: './trip-history.component.html',
   styleUrl: './trip-history.component.css'
 })
