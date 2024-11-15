dec3bf0a96adb9a424b7211ced88eb59b56efcd5
rivacortez <rivaldo062134@gmail.com>
2024-11-14 23:34:06 -0500

    feat(service): add request & status service

---
+++ src/app/request-service/service/request.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import {BaseService} from "../../../shared/services/base.service";
+import {RequestServiceEntity} from "../model/request-service.entity";
+import {catchError, Observable, Subject, tap, throwError} from "rxjs";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {NotificationService} from "./notification.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RequestService extends BaseService<RequestServiceEntity> {
+  constructor(http: HttpClient, private notificationService: NotificationService) {
+    super();
+    this.http = http;
+    this.resourceEndpoint = '/requestServiceTrips';
+  }
+  getAllRequests(): Observable<RequestServiceEntity[]> {
+    return this.http.get<RequestServiceEntity[]>(this.resourcePath()).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  saveRequestServiceTrip(trip: RequestServiceEntity): Observable<any> {
+    return this.http.post(this.resourcePath(), trip, this.httOptions).pipe(
+      tap(() => this.notificationService.showNotification(
+        'Solicitud guardada',
+        'The trip has been saved successfully.',
+        'success',
+        'path/to/success-image.jpg'
+      )),
+      catchError(this.handleError)
+    );
+  }
+
+  override handleError(error: HttpErrorResponse) {
+    console.error('Error saving trip', error);
+    return throwError(() => new Error('Error saving trip'));
+  }
+
+  private acceptedTrips: Set<number> = new Set<number>();
+
+  addTrip(tripId: number): void {
+    this.acceptedTrips.add(tripId);
+  }
+
+  hasTrip(tripId: number): boolean {
+    return this.acceptedTrips.has(tripId);
+  }
+
+  rejectRequest(id: number): Observable<any> {
+    const url = `${this.basePath}/status`;
+    const statusUpdate = { id: id, status: 'rejected', requestServiceTripId: id };
+    return this.http.post(url, statusUpdate, this.httOptions).pipe(
+      tap(() => this.notificationService.showNotification(
+        'Solicitud rechazada',
+        'The trip you requested has been rejected.',
+        'error',
+        'path/to/error-image.jpg'
+      )),
+      catchError(this.handleError)
+    );
+  }
+
+
+
+}
---
+++ src/app/request-service/service/status.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
+import {catchError, Observable, throwError} from "rxjs";
+import {StatusEntity} from "../model/status.entity";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StatusService {
+  private basePath = `${environment.serverBasePath}/status`;
+
+  constructor(private http: HttpClient) {}
+
+  getAllStatuses(): Observable<StatusEntity[]> {
+    return this.http.get<StatusEntity[]>(this.basePath).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occurred!';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `An error occurred: ${error.error.message}`;
+    } else {
+      errorMessage = `Backend returned code ${error.status}, body was: ${JSON.stringify(error.error)}`;
+    }
+    console.error(errorMessage);
+    return throwError(() => new Error(errorMessage));
+  }
+}
