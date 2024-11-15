87e477ef46c9f51abf46998a7ccd74153224e85a
rivacortez <rivaldo062134@gmail.com>
2024-11-14 23:56:47 -0500

    feat(public): add images

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  serverBasePath: 'http://localhost:3000/api/v1'
+  //serverBasePath:'https://cargosafe-fake-api.vercel.app'
+  serverBasePath:'http://localhost:3000'
+  //serverBasePath:'http://localhost:8080'
 };
